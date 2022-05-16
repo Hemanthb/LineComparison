66c5cb352ce09190988e6ef9d58d72a0f6c7bbc2
hem_b <hemanthb93@gmail.com>
2022-05-16 12:10:39 +0530

    [Hemanth]: calculating length of a line

---
+++ LineComparison/LineComparison/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+LineComparison.Line linecomparison = new LineComparison.Line();
+double length = linecomparison.getLength(2, 3, 2, 8);
+Console.WriteLine("length of the given line is"+length);
